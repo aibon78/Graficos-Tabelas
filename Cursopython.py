4b680ed9d8abdb616ec3e201b71b6a1c0f2a376a
André Filipe <130567849+aibon78@users.noreply.github.com>
2024-06-07 20:30:32 -0300

    Update Cursopython.py

--- Cursopython.py
+++ Cursopython.py
@@ -13,7 +13,7 @@ for arquivo in lista_de_arquivos:
         tabela = pd.read_csv(f"C:\\Users\\Andre\\Desktop\\Vendas\\{arquivo}")
         tabela_total = tabela_total.append(tabela)
 display(tabela_total)
-
+.
 # Tratar / Compilar base de dados
 # Passo 4 - Calcular o produto mais vendido (em quantidade)
 # Passo 5 - Calcular o produto que mais faturo (em faturamento)
