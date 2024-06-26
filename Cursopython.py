456e2a4f455d86c701e3827d29f5a1ff59464e29
André Filipe <130567849+aibon78@users.noreply.github.com>
2024-06-26 15:55:30 -0300

    Update Cursopython.py

--- Cursopython.py
+++ Cursopython.py
@@ -13,10 +13,14 @@ for arquivo in lista_de_arquivos:
         tabela = pd.read_csv(f"C:\\Users\\Andre\\Desktop\\Vendas\\{arquivo}")
         tabela_total = tabela_total.append(tabela)
 display(tabela_total)
-.
-# Tratar / Compilar base de dados
+
+
+
 # Passo 4 - Calcular o produto mais vendido (em quantidade)
+'''tabela_produto = tabela_total.groupby('produtos')'''
+
 # Passo 5 - Calcular o produto que mais faturo (em faturamento)
+
 # Calcular a cidade/loja que mais vendeu (em faturamento) - criar um grafico/dashboard
 
 
